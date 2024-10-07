f24292a8673075ab9790eb23c7449c2a3d735429
VedangWajge <64345981+VedangWajge@users.noreply.github.com>
2024-10-07 10:22:18 +0530

    Update Q3Assignment3.js

--- Q3Assignment3.js
+++ Q3Assignment3.js
@@ -1,3 +1,26 @@
+class Person {
+    constructor(personName, age) {
+        this.personName = personName;
+        this.age = age;
+    }
+
+    printDetails() {
+        console.log(`${this.personName} is ${this.age} years old`);
+    }
+
+    setDetails(personName, age) {
+        this.personName = personName;
+        this.age = age;
+    }
+
+    checkAdult() {
+        if (this.age >= 18) {
+            console.log(`${this.personName} is an adult`);
+        }
+    }
+}
+
+
 class Student extends Person {
     constructor(personName, age, division) {
         super(personName, age);
